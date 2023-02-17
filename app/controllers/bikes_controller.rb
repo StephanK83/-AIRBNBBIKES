25663f16fe05e55cde53d4479e222f209eea182f
Lorenzo <lorenzo.bertolino@gmail.com>
2023-02-17 20:44:26 +0100

    bike controller actions done until show

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -9,8 +9,19 @@ class BikesController < ApplicationController
 
   def create
     @bike = Bike.new(bike_params)
+    @bike.user = current_user
     @bike.save
   end
+
+  def show
+    @bike = bike_params[:id] #not sure if it's right
+  end
+
+  def edit
+    
+  end
+
+
 
   private
 
