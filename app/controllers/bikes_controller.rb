31d8b967e066c75bee9c95ffc23a940b45d87e2a
Lorenzo <lorenzo.bertolino@gmail.com>
2023-02-17 20:33:56 +0100

    bike controller methods done until create

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -2,4 +2,20 @@ class BikesController < ApplicationController
   def index
     @bike = Bike.all
   end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.save
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:brand, :color, :price)
+  end
+
 end
