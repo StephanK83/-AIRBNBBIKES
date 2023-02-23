bfeaed3dd2b3b51667cb69f4fbe2a532333a65d4
Stephan Knorn <stephan.knorn@berlin101.de>
2023-02-23 22:10:18 +0100

    added cloudinary

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  
+  has_one_attached :photo
   validates :size, presence: true 
   validates :user, presence: true
   validates :brand, presence: true
