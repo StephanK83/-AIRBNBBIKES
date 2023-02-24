4804666623fe4d850a42639411035cf9554258b6
cipriancrisan <ciprian.d.crisan@gmail.com>
2023-02-24 22:29:26 +0100

    index html not finished yet

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,9 +2,11 @@ class Bike < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
-  validates :size, presence: true 
+
+  validates :size, presence: true
   validates :user, presence: true
   validates :brand, presence: true
   validates :color, presence: true
   validates :price, presence: true
+  validates :photo, presence: true
 end
